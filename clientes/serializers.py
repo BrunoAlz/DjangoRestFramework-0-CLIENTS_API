f98abc7a2395ace0eb1cc3168e4639c87982dfa2
Bruno <bruno6821@gmail.com>
2021-10-13 19:20:11 -0300

    Valindando Tamanho do CPF

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -5,4 +5,8 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+    """VALIDANDO TAMANHO DO CPF"""
+    def validade_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError('O CPF deve ter 11 digitos.')
+        return cpf
