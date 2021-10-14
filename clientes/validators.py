99282e070713569b6d372e824b52feb0ae9a30d0
Bruno <bruno6821@gmail.com>
2021-10-13 21:06:00 -0300

    Validando CPF com REGRAS NACIONAS

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,9 +1,11 @@
 import re
 from rest_framework import serializers
+from validate_docbr import CPF
 
 
 def cpf_valido(numero_do_cpf):
-    return len(numero_do_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
 
 
 def nome_valido(nome):
